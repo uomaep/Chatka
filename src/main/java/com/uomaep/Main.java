ea2a633d083a6423f86edcdcddb1ddfa12736528
uomaep <uomaep@gmail.com>
2024-03-17 02:06:24 +0900

    initial commit

--- src/main/java/com/uomaep/Main.java
+++ src/main/java/com/uomaep/Main.java
@@ -1,11 +1,6 @@
 package com.uomaep;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws InterruptedException {
 
     }
